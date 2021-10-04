96d2893039949a45843cb7c7fa544fba700a3f73
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-04 13:46:44 +0100

    ft: add in comments

--- MongoUrlParser/Parser.cs
+++ MongoUrlParser/Parser.cs
@@ -4,13 +4,18 @@ namespace MongoUrlParser
 {
     public static class Parser
     {
+        /// <summary>
+        /// Parse a mongodb url and extract the connection string and the database name
+        /// </summary>
+        /// <param name="mongoUrl">A mongodb connection url with a database name</param>
+        /// <returns>A value tuple with the connection first and the database name next</returns>
         public static (string databaseUrl, string databaseName) Parse(string mongoUrl)
         {
             var url = new Uri(mongoUrl, UriKind.Absolute);
 
             var output = $"{url.Scheme}://{url.UserInfo}@{url.Host}";
 
-            if (url.Port > 0 )
+            if (url.Port > 0)
             {
                 output += $":{url.Port}";
             }
